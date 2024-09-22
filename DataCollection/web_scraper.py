053c53ee56f3258a57c40da336fef809d814bd3a
fatimashehab99 <fatima_shehab_sh@outlook.com>
2024-09-22 16:37:06 +0300

    gcp updates

--- DataCollection/web_scraper.py
+++ DataCollection/web_scraper.py
@@ -1,6 +1,13 @@
 from article_scraper import getArticleContent
 from article_storage import storeToMongoDB
 from article_scraper import getArticles
+from google.cloud import bigquery
+
+# Create a BigQuery client
+client = bigquery.Client()
+
+# Specify your BigQuery table (project.dataset.table)
+table_id = "apt-nebula-425921-i1.data_engine.articles"
 
 
 def main():
@@ -10,8 +17,16 @@ def main():
     articles_url = getArticles("https://www.almayadeen.net/sitemaps/all/sitemap-2024-8.xml")
     # get article data ans store it in mongo DB
     for article_url in articles_url:
-        article_data.append(getArticleContent(article_url))
-    storeToMongoDB(article_data)
+        i+=1
+        gcp = (getArticleContent(article_url))
+        errors=client.insert_rows_json(table_id, [gcp])
+        print(i)
+        if errors:
+            print(f"Encountered errors while inserting rows: {errors}")
+        else:
+            print("Row successfully inserted.")
+
+    # storeToMongoDB(article_data)
 
 
 if __name__ == "__main__":
